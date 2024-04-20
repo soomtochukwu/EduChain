e74f140443b3fa412fd2d3972d99a85c0685060e
MaZi <secondarymazi@gmail.com>
2024-04-20 13:53:41 +0100

    edwed

--- Smartcontract/hardhat.config.js
+++ Smartcontract/hardhat.config.js
@@ -7,9 +7,27 @@ module.exports = {
   networks: {
     baobab: {
       url: "https://klaytn-baobab.blockpi.network/v1/rpc/3fd614df10af5487e67aa62d97bc114b51f3a706",
-      gasPrice: 250000000000,
+      chainId: 1001,
       accounts: ["0389d72491ccb7092bcd453fd96d28f553820bfac9c59b069efd61ddc0010944"],
       blockConfirmations: 6,
     }
+  },
+  etherscan: {
+    apiKey: {
+      baobab: "unnecessary",
+    },
+    customChains: [
+      {
+        network: "baobab",
+        chainId: 1001,
+        urls: {
+          apiURL: "https://api-baobab.klaytnscope.com/api",
+          browserURL: "https://baobab.klaytnscope.com",
+        },
+      },
+    ]
+  },
+  sourcify: {
+    enabled: false
   }
 };
