88a95cd63ad71c72b7d03baa70c2e2ed1a541e10
MaZi <secondarymazi@gmail.com>
2024-05-02 13:29:41 +0100

    ++++++++++++++

--- frontend/app/(schoolAdmin)/admin/layout.tsx
+++ frontend/app/(schoolAdmin)/admin/layout.tsx
@@ -30,8 +30,9 @@ export default function SchoolAdminLayout({
     // status !== "connected" ? router.push("/") : null;
     // status == "connected" ? router.push("/admin") : null;
     address !==
-    ("0x49f2451AbEe35B261bB01f9d0CDC49f8f8df6E3f" ||
-      "0x6CD128fca70c1011A00a58f410BDaFE31A5D6cFc")
+    ("0x49f2451AbEe35B261bB01f9d0CDC49f8f8df6E3f" &&
+      "0x4d5fea37e8a7597fE57e6dd967B483056A488eFB" &&
+      "0xE122199bB9617d8B0e814aC903042990155015b4")
       ? router.back()
       : null;
   }, [status]);
